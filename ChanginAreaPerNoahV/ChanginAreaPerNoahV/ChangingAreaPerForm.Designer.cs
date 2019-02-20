38c5daeb64eb2d925c6698579fd423d7dc98170d
Noah Voroney <noah.voroneyocsbstudent.ca>
2019-02-20 12:36:09 -0500

    added circumference daily asignment

--- ChanginAreaPerNoahV/ChanginAreaPerNoahV/ChangingAreaPerForm.Designer.cs
+++ ChanginAreaPerNoahV/ChanginAreaPerNoahV/ChangingAreaPerForm.Designer.cs
@@ -28,21 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmChangingAreaPer));
+            this.txtBoxLength = new System.Windows.Forms.TextBox();
+            this.txtBoxWidth = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblPerimiterAnswer = new System.Windows.Forms.Label();
+            this.lblAreaAnswer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPer = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtBoxLength
+            // 
+            this.txtBoxLength.Location = new System.Drawing.Point(158, 89);
+            this.txtBoxLength.Name = "txtBoxLength";
+            this.txtBoxLength.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxLength.TabIndex = 0;
+            this.txtBoxLength.TextChanged += new System.EventHandler(this.txtBoxLength_TextChanged);
+            // 
+            // txtBoxWidth
+            // 
+            this.txtBoxWidth.Location = new System.Drawing.Point(158, 130);
+            this.txtBoxWidth.Name = "txtBoxWidth";
+            this.txtBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxWidth.TabIndex = 1;
+            this.txtBoxWidth.TextChanged += new System.EventHandler(this.txtWidth_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(539, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(74, 137);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(53, 13);
+            this.lblWidth.TabIndex = 3;
+            this.lblWidth.Text = "Width (M)";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(74, 89);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(58, 13);
+            this.lblLength.TabIndex = 4;
+            this.lblLength.Text = "Length (M)";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(279, 109);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblPerimiterAnswer
+            // 
+            this.lblPerimiterAnswer.AutoSize = true;
+            this.lblPerimiterAnswer.Location = new System.Drawing.Point(223, 207);
+            this.lblPerimiterAnswer.Name = "lblPerimiterAnswer";
+            this.lblPerimiterAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblPerimiterAnswer.TabIndex = 6;
+            // 
+            // lblAreaAnswer
+            // 
+            this.lblAreaAnswer.AutoSize = true;
+            this.lblAreaAnswer.Location = new System.Drawing.Point(223, 236);
+            this.lblAreaAnswer.Name = "lblAreaAnswer";
+            this.lblAreaAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAreaAnswer.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // lblPer
+            // 
+            this.lblPer.AutoSize = true;
+            this.lblPer.Location = new System.Drawing.Point(97, 196);
+            this.lblPer.Name = "lblPer";
+            this.lblPer.Size = new System.Drawing.Size(47, 13);
+            this.lblPer.TabIndex = 10;
+            this.lblPer.Text = "Perimiter";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(97, 236);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(29, 13);
+            this.lblArea.TabIndex = 11;
+            this.lblArea.Text = "Area";
             // 
             // frmChangingAreaPer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(539, 366);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblPer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAreaAnswer);
+            this.Controls.Add(this.lblPerimiterAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.txtBoxWidth);
+            this.Controls.Add(this.txtBoxLength);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmChangingAreaPer";
             this.Text = "Changing Area and Perimiter";
             this.Load += new System.EventHandler(this.ChangingAreaPerForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBoxLength;
+        private System.Windows.Forms.TextBox txtBoxWidth;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblPerimiterAnswer;
+        private System.Windows.Forms.Label lblAreaAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPer;
+        private System.Windows.Forms.Label lblArea;
     }
 }
 
--- ChanginAreaPerNoahV/ChanginAreaPerNoahV/ChangingAreaPerForm.cs
+++ ChanginAreaPerNoahV/ChanginAreaPerNoahV/ChangingAreaPerForm.cs
@@ -21,5 +21,40 @@ namespace ChanginAreaPerNoahV
         {
 
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declare local variables
+            double length, width, area, perimiter;
+
+            // convert to a string from  each text box to a double
+            length = double.Parse(txtBoxLength.Text);
+            width = double.Parse(txtBoxWidth.Text);
+
+            //Calculate the Area and Perimiter\
+            area = length * width;
+            perimiter = 2 * (length + width);
+
+            //insert the answers into the boxes
+            this.lblAreaAnswer.Text = Convert.ToString(area) + " Squared meters";
+            this.lblPerimiterAnswer.Text = Convert.ToString(perimiter) + " meters";
+
+                // display the answer boxes
+        }
+
+        private void txtBoxLength_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
